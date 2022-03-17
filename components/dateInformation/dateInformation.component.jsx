8cf62f8606a80f86ea65ca17c0711be56875ae80
Ruslan Khabachou <ruslank@criticalmass.com>
2022-03-17 16:50:41 -0400

    fix for ssr-no-token.tsx

--- components/dateInformation/dateInformation.component.jsx
+++ components/dateInformation/dateInformation.component.jsx
@@ -1,11 +1,10 @@
-import { formatDistanceToNow } from "date-fns";
 
 const DateInformation = ({createAt}) => {
 	return (
     <>
 			<div className="bg-time-img w-5 h-5 bg-no-repeat bg-center mr-1"></div>
 			<p className="text-font-placeholder truncate text-xs font-medium">
-				{ formatDistanceToNow(new Date(createAt))} {" "} ago.
+				ago.
 			</p>
 		</>
 	);
